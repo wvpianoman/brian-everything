19e0b83e0e8478f655110c9f804811ae7355d63d
Brian F <dbf.linux@gmail.com>
2024-03-19 20:32:22 -0400

    error fix

--- ARCO/1-PACKAGES/packages.sh
+++ ARCO/1-PACKAGES/packages.sh
@@ -31,17 +31,27 @@ echo "Installing Essential Software Packages"
 
 sudo pacman -Sy acl akonadi akonadi-calendar-tools akonadi-calendar akonadi-import-wizard aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates ca-certificates-mozilla ca-certificates-utils cifs-utils cjson codec2 cowsay curl dbus-glib dconf-editor dialog direnv
 
-sudo pacman -Sy duf earlyoom easyeffects espeak-ng fancontrol-gui-git fastfetch fd ffmpeg ffmpegthumbnailer ffmpegthumbs figlet flatpak fortune-mod git grsync gnupg noto-fonts-emoji noto-fonts noto-color-emoji-fontconfig grep grub-customizer gstreamer-vaapi gstreamer gtk-murrine-engine
+sleep 1
 
-sudo pacman -Sy gtk-engines haveged htop intel-media-driver iptables jq kate kdegraphics-thumbnailers kdepim libffi libfreeaptx librabbitmq librist libsodium libtool libva-intel-driver libvdpau libvdpau-va-gl libXext llvm16-libs lpcnetfreedv lsd make mbedtls meld
+sudo pacman -Sy duf earlyoom easyeffects espeak-ng fancontrol-gui-git fastfetch fd ffmpeg ffmpegthumbnailer ffmpegthumbs figlet flatpak fortune-mod git grsync gnupg grep grub-customizer gstreamer-vaapi gstreamer gtk-engine-murrine
 
-sudo pacman -Sy firefox-ublock-origin merkuro mpg123 nano nano-syntax-highlighting neofetch neovim neovim-qt net-snmp net-tools nftables nsxiv openssh ostree p7zip p7zip-gui packagekit pip pkgconf plasma-firewall plocate powertop pulseeffects python python-pip
+sleep 1
 
-sudo pacman -Sy python-setproctitle qrencode rclone rclone-browser ripgrep ripgrep-all rsync rygel sassc screen socat openssl sshpass sxiv tar terminator tlp tlp-rdw
+sudo pacman -Sy gtk-engines gum haveged htop intel-media-driver iptables jq kate kdegraphics-thumbnailers libffi libfreeaptx rabbitmq librist libsodium libtool libva-intel-driver libvdpau libvdpau-va-gl libxext llvm16-libs lsd make mbedtls meld
 
-sudo pacman -Sy tlpui tumbler ufw ufw-extras ugrep un{zip,rar} unrar-free variety vim virt-manager wget wsdd xclip xorg-fonts-100dpi yakuake zip zramswap zram-generator zstd
+sleep 1
 
-sleep 3
+sudo pacman -Sy firefox-ublock-origin merkuro mpg123 nano nano-syntax-highlighting neofetch neovim neovim-qt net-snmp net-tools nftables nsxiv openssh ostree p7zip p7zip-gui packagekit pkgconf plasma-firewall plocate powertop pulseeffects python python-pip
+
+sleep 1
+
+sudo pacman -Sy python-setproctitle qrencode ripgrep ripgrep-all rsync rygel sassc screen socat openssl sshpass sxiv tar terminator tlp tlp-rdw
+
+sleep 1
+
+sudo pacman -Sy tlpui tumbler ufw ufw-extras ugrep un{zip,rar} unrar-free variety vim virt-manager wget xclip yakuake zip zramswap zram-generator zstd
+
+sleep 1
 
 # read -n 1 -r -s -p $'Press enter to continue...\n'
 
@@ -51,9 +61,9 @@ sleep 3
 
 # read -n 1 -r -s -p $'Press enter to continue...\n'
 
-echo "Installiong Software Packages"
+echo "Installing Software Packages"
 
-sudo pacman -Sy blender boomaga digikam flameshot ghostwriter gimp gimp-help gimp-extras inkscape kdepim kdepim-addons krita neochat rhythmbox scribus shotwell simplescreenrecorder syncthing transmission transmission-{cli,remote-gtk,gtk,qt} uget vlc yakuake
+sudo pacman -Sy blender digikam flameshot ghostwriter gimp gimp-help-en gimp-extras inkscape kdepim-addons krita neochat rclone rclone-browser rhythmbox scribus shotwell simplescreenrecorder syncthing transmission-{cli,remote-gtk,gtk,qt} uget vlc yakuake
 
 echo "Package installation completed."
 
@@ -63,7 +73,7 @@ sleep 3
 
 echo "Installing utilites for different file system access"
 
-sudo pacman -Sy btrfs-progs bfrs-assistant grub-btrfs btrfsmaintenance exfatprogs exfat-utils f2fs-tools hfsprogs jfsutils lvm2 nilfs-utils ntfs-3g udftools xfsprogs
+sudo pacman -Sy btrfs-progs btrfs-assistant grub-btrfs btrfsmaintenance exfatprogs exfat-utils f2fs-tools hfsprogs jfsutils lvm2 nilfs-utils ntfs-3g udftools xfsprogs
 
 echo "Package installation completed."
 
@@ -76,7 +86,7 @@ sudo systemctl enable --now earlyoom
 
 # Installing fonts
 
-sudo apt install ttf-font-awesome awesome-terminal-fonts powerline-fonts ttf-fira-code
+sudo pacman -Sy ttf-font-awesome awesome-terminal-fonts powerline-fonts ttf-fira-code xorg-fonts-100dpi
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 sudo unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
@@ -148,10 +158,10 @@ rm ./FiraCode.zip ./Meslo.zip ./WPS-FONTS.zip
 	sudo systemctl enable fstrim.timer
 #################################################
 
+
 	# Audio
 	[ -f /usr/bin/easyeffects ] && [ -f $HOME/.config/easyeffects/output/default.json ] && easyeffects -l default
 	[ -f /usr/bin/pulseeffects ] && [ -f $HOME/.config/PulseEffects/output/default.json ] && pulseeffects -l default
------------------------------------------------
 
 	# Configure fortune
 	# If you want to display a specific fortune file or category, you can use the -e option followed by the file or category name. For example:
@@ -163,22 +173,16 @@ rm ./FiraCode.zip ./Meslo.zip ./WPS-FONTS.zip
 	/usr/bin/rygel-preferences
 
 	# Install profile-sync: it to manage browser profile(s) in tmpfs and to periodically sync back to the physical disc (HDD/SSD)
-	sudo dnf install profile-sync-daemon
+
+	sudo pacman -Sy install profile-sync-daemon
 	/usr/bin/profile-sync-daemon preview
 	# sudo dnf remove profile-sync-daemon
 	# psd profile located in $HOME/.config/psd/psd.conf
 
-
-	## Make a backup of the original sysctl.conf file
-	display_message "[${GREEN}✔${NC}]  Tweaking network settings"
-
-	cp $SYS_PATH /etc/sysctl.conf.bak
-
-	echo
-	yellow_msg 'Default sysctl.conf file Saved. Directory: /etc/sysctl.conf.bak'
-	echo
-	gum spin --spinner dot --title "Stand-by..." -- sleep 1
-
+# Function to print text in yellow color
+print_yellow() {
+	echo -e "\e[93m$1\e[0m"
+}
 
 # Function to clear systemd journal logs
 function clear_journal_logs() {
@@ -190,10 +194,10 @@ function clear_journal_logs() {
 echo -e "\n\n----------------------------------------------"
 echo -e "|     Let's clean up                         |"
 echo -e "----------------------------------------------\n\n"
-sudo update-grub
-sudo dnf -y up
-sudo dnf -y autoremove
-sudo dnf -y clean all
+#sudo update-grub
+#sudo dnf -y up
+#sudo dnf -y autoremove
+#sudo dnf -y clean all
 clear_journal_logs
 remove_residual_config_files
 
@@ -204,8 +208,8 @@ sudo fstrim -av
 
 echo -e "\n\n----------------------------------------------"
 echo -e "|                                            |"
-echo -e "|      Setup Complete! Enjoy Ultramarine     |"
-echo -e "|       Please run ___________.sh            |"
+echo -e "|               Setup Complete!              |"
+echo -e "|          Please run ___________.sh         |"
 echo -e "|    to back up your APT packages and more   |"
 echo -e "|                                            |"
 echo -e "----------------------------------------------\n\n"
@@ -234,22 +238,3 @@ is_service_enabled() {
 	systemctl is-enabled "$1" &>/dev/null
 }
 
-# Function to print text in yellow color
-print_yellow() {
-	echo -e "\e[93m$1\e[0m"
-}
-
-install_apps() {
-	display_message "[${GREEN}✔${NC}]  Installing afew personal apps..."
-
-	# Enable trim support
-	sudo systemctl enable fstrim.timer
-
-	# Execute rygel to start DLNA sharing
-	/usr/bin/rygel-preferences
-
-	# Install profile-sync: it to manage browser profile(s) in tmpfs and to periodically sync back to the physical disc (HDD/SSD)
-	sudo dnf install profile-sync-daemon
-	/usr/bin/profile-sync-daemon preview
-	# sudo dnf remove profile-sync-daemon
-	# psd profile located in $HOME/.config/psd/psd.conf
