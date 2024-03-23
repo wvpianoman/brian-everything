dc12b4317eef7db80716f04ef0d453f9b9044188
Brian F <130115651+wvpianoman@users.noreply.github.com>
2024-03-23 01:50:07 +0000

    Update packages.sh

--- ARCO/1-PACKAGES/packages.sh
+++ ARCO/1-PACKAGES/packages.sh
@@ -29,7 +29,11 @@ echo "Installing Essential Software Packages"
 
 # to use on CachyOS to install Arco keys and tools
 
-#wget bit.ly/get-arcolinux-keys && chmod +x ./get-arcolinux-keys  && sudo ./get-arcolinux-keys && sudo pacman -Syu archlinux-tweak-tool-git
+wget bit.ly/get-arcolinux-keys && chmod +x ./get-arcolinux-keys  && sudo ./get-arcolinux-keys && sudo pacman -Syu archlinux-tweak-tool-git
+
+read -n 1 -r -s -p $'Press enter to continue...\n'
+
+sudo pacman -Sy arcolinux-meta-btrfs
 
 sudo pacman -Sy acl akonadi akonadi-calendar-tools akonadi-calendar akonadi-import-wizard aria2 attr autoconf automake bash-completion bc binutils btop busybox ca-certificates ca-certificates-mozilla ca-certificates-utils cifs-utils cjson codec2 cowsay curl dbus-glib dconf-editor dialog direnv
 
@@ -39,7 +43,7 @@ sudo pacman -Sy discover duf earlyoom easyeffects espeak-ng fancontrol-gui-git f
 
 sleep 2
 
-sudo pacman -Sy gtk-engines gum haveged htop intel-media-driver iptables jq kate kdegraphics-thumbnailers libffi libfreeaptx rabbitmq librist libsodium libtool libva-intel-driver libvdpau libvdpau-va-gl libxext llvm16-libs lsd make mbedtls meld
+sudo pacman -Sy gtk-engines gum haveged htop intel-media-driver iptables-nft jq kate kdegraphics-thumbnailers libffi libfreeaptx rabbitmq librist libsodium libtool libva-intel-driver libvdpau libvdpau-va-gl libxext llvm16-libs lsd make mbedtls meld
 
 sleep 2
 
